017b7867b8260d9acdc240c3e0d98366968a9233
Tatiana-Tsymbaliuk <114348739+Tatiana-Tsymbaliuk@users.noreply.github.com>
2023-05-06 18:34:38 +0300

    ghjgjg

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,14 @@
-import { PageTitle } from 'components/PageTitle/PageTitle';
-import { EventBoard } from 'components/EventBoard/EventBoard';
-import upcomingEvents from '../upcoming-events.json';
+// import { PageTitle } from 'components/PageTitle/PageTitle';
+// import { EventBoard } from 'components/EventBoard/EventBoard';
+// import upcomingEvents from '../upcoming-events.json';
+import Counter from 'components/Counter/Counter';
 
 export const App = () => {
   return (
     <div>
-      
-      <PageTitle text="24th Core World Coalition Conference" />
-      <EventBoard events={upcomingEvents} />
+      <Counter/>
+      {/* <PageTitle text="24th Core World Coalition Conference" />
+      <EventBoard events={upcomingEvents} /> */}
       
     </div>
   );
