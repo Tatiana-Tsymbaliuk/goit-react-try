7eda8946d71aa683dca2407c0412229253983427
Tatiana-Tsymbaliuk <114348739+Tatiana-Tsymbaliuk@users.noreply.github.com>
2023-05-07 15:44:56 +0300

    fdjfkdjfks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,14 @@
-// import { PageTitle } from 'components/PageTitle/PageTitle';
-// import { EventBoard } from 'components/EventBoard/EventBoard';
-// import upcomingEvents from '../upcoming-events.json';
-import Counter from 'components/Counter/Counter';
+import { PageTitle } from 'components/PageTitle/PageTitle';
+import { EventBoard } from 'components/EventBoard/EventBoard';
+import upcomingEvents from '../upcoming-events.json';
+
 
 const App = () => {
   return (
     <div>
-      <Counter/>
-      {/* <PageTitle text="24th Core World Coalition Conference" /> */}
-      {/* <EventBoard events={upcomingEvents} /> */}
+      
+      <PageTitle text="24th Core World Coalition Conference" />
+      <EventBoard events={upcomingEvents} />
       
     </div>
   );
